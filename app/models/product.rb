fa6861f937d5c76c147146ebb0ce3bf9df8f72e8
dendiflet <bretin.iannis@gmail.com>
2019-03-04 12:34:24 +0100

    ajout de presence et uniqueness dans les model + test users et products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+	validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true, uniqueness: true
+
+
+
+
 end
