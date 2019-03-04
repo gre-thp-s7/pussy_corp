c2646ad42c929e3ecc6b3333c9810d1895977f20
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-03-04 16:12:49 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { :host => 'https://pussy-corp.herokuapp.com/' }
+  
+  
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
