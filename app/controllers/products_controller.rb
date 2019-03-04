50c106a237eb40a9a0ce4908b3c04fbbd28d73e2
Carlaight <45879823+Carlaight@users.noreply.github.com>
2019-03-04 16:08:56 +0100

    Create products_controller.rb

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+    # GET /users
+    def index
+      @products = Prodruct.all
+    end
+
+    # GET /users/1
+    def show
+    end
+
+    # GET /users/1/edit
+    def edit
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_product
+        @product = Product.find(params[:id])
+      end
+end
