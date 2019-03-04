6bfa6e9619d6ea8591d6c0b927fe0b21bfdb7853
JungleWilly <guillaumec.dev@gmail.com>
2019-03-04 17:58:06 +0100

    add views/products/show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
   
     # GET /users/1
     def show
+
     end
   
     # GET /users/1/edit
@@ -17,6 +18,6 @@ class ProductsController < ApplicationController
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_product
-        @product = Product.find(params[:id])
+        # @product = Product.find(params[:id])
       end
 end
