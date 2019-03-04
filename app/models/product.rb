69311675c9aa0ec6ae3a96c305b447aba7129c53
carlaight <charlesmix23@gmail.com>
2019-03-04 12:04:02 +0100

    add validates product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
 end
